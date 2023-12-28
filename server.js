1dc5da598685f023134dbcb28eac0146daa5ce89
mackwick <mackenziewicker@gmail.com>
2023-12-28 12:40:52 -0500

    server set up and listening

---
+++ server.js
@@ -0,0 +1,14 @@
+//DEPENDENCIES
+const express = require("express")
+const app = express()
+
+//MIDDLEWARE
+
+
+//ROUTES
+
+
+//SERVER LISTENING
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
